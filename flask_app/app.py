b778b4e486e1eb9d32c6ff1fb364ebc76db194b3
EmmaKozmer <emma.kozmer@student.unisg.ch>
2023-11-09 22:06:41 +0100

    updated with function to fill tables

--- flask_app/app.py
+++ flask_app/app.py
@@ -2,10 +2,11 @@ import os
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from flask import Flask, render_template, request, url_for, redirect
-
+import logging
 
 basedir = os.path.abspath(os.path.dirname(__file__)) # get the base directory of the project
 app = Flask(__name__) # create the Flask app
+app.logger.setLevel(logging.DEBUG)
 
 DATABASE_NAME = 'real_estate_db' # name of the database to connect to
 
@@ -17,27 +18,6 @@ DATABASE_URL = POSTGRES_URL + '/' + DATABASE_NAME
 def get_db_connection():
     conn = psycopg2.connect(DATABASE_URL)
     return conn
-
-# Function to create a new database in PostgreSQL
-def create_database(db_name):
-    # Connection string to connect with the default 'postgres' database
-    conn_string = POSTGRES_URL + '/postgres'
-
-    # Connect to the PostgreSQL server
-    conn = psycopg2.connect(conn_string)
-    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)  # <-- AUTOCOMMIT
-    cursor = conn.cursor()
-
-    # Check if the database already exists
-    cursor.execute(psycopg2.sql.SQL("SELECT 1 FROM pg_catalog.pg_database WHERE datname = %s"), (db_name,))
-    exists = cursor.fetchone()
-
-    # If the database does not exist, then create it
-    if not exists:
-        cursor.execute(psycopg2.sql.SQL("CREATE DATABASE {}").format(psycopg2.sql.Identifier(db_name)))
-
-    cursor.close()
-    conn.close()
 
 # Function to create the database tables
 def setup_db():
@@ -80,27 +60,117 @@ def setup_db():
             first_name VARCHAR(255) NOT NULL,
             last_name VARCHAR(255) NOT NULL,
             date_of_birth DATE NOT NULL,
-            phone_number VARCHAR(255) NOT NULL,
-            address VARCHAR(255) NOT NULL
+            phone_number VARCHAR(255) NOT NULL
         );
     ''')
     cur.execute('''
-        CREATE TABLE IF NOT EXISTS contracts (
-            id SERIAL PRIMARY KEY,
-            property_id INTEGER NOT NULL,
-            client_id INTEGER NOT NULL,
-            owner_id INTEGER NOT NULL,
-            agent_id INTEGER NOT NULL,
-            price INTEGER NOT NULL,
-            FOREIGN KEY (property_id) REFERENCES properties (id),
-            FOREIGN KEY (client_id) REFERENCES clients (id),
-            FOREIGN KEY (owner_id) REFERENCES owners (id),
-            FOREIGN KEY (agent_id) REFERENCES agents (id)
+    CREATE TABLE IF NOT EXISTS contracts (
+        id SERIAL PRIMARY KEY,
+        property_id INTEGER NOT NULL,
+        client_id INTEGER NOT NULL,
+        owner_id INTEGER NOT NULL,
+        agent_id INTEGER NOT NULL,
+        price INTEGER NOT NULL,
+        FOREIGN KEY (property_id) REFERENCES properties (id) ON DELETE CASCADE,
+        FOREIGN KEY (client_id) REFERENCES clients (id) ON DELETE SET NULL,
+        FOREIGN KEY (owner_id) REFERENCES owners (id) ON DELETE SET NULL,
+        FOREIGN KEY (agent_id) REFERENCES agents (id) ON DELETE SET NULL
         );
     ''')
     conn.commit()
     cur.close()
     conn.close()
+
+
+# Function to create a new database in PostgreSQL
+def create_database(db_name):
+    # Connection string to connect with the default 'postgres' database
+    conn_string = POSTGRES_URL + '/postgres'
+
+    # Connect to the PostgreSQL server
+    conn = psycopg2.connect(conn_string)
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)  # <-- AUTOCOMMIT
+    cursor = conn.cursor()
+
+    # Check if the database already exists
+    cursor.execute("SELECT 1 FROM pg_catalog.pg_database WHERE datname = %s", (db_name,))
+    exists = cursor.fetchone()
+
+    if exists:
+        cursor.execute(f"DROP DATABASE {db_name}")
+
+    # If the database does not exist, then create it
+    cursor.execute(f"CREATE DATABASE {db_name}")
+
+    cursor.close()
+    conn.close()
+
+# fill database
+def seed_db():
+    print(11111)
+    try:
+        # Establish a connection to the database
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        # Insert data into the properties table
+        cur.execute('''
+        INSERT INTO properties (location, size, rooms, building_year)
+        VALUES
+        ('123 Main St', 1000, 3, 1990),
+        ('456 Elm St', 1500, 4, 1970),
+        ('789 Maple St', 2000, 5, 1985);
+        ''')
+
+        # Insert data into the clients table
+        cur.execute('''
+        INSERT INTO clients (first_name, last_name, date_of_birth, phone_number, address)
+        VALUES
+        ('John', 'Doe', '1980-01-01', '1234567890', '123 Main St'),
+        ('Jane', 'Smith', '1990-05-15', '0987654321', '456 Elm St');
+        ''')
+
+        # Insert data into the owners table
+        cur.execute('''
+        INSERT INTO owners (first_name, last_name, date_of_birth, phone_number, address)
+        VALUES
+        ('Alice', 'Johnson', '1975-09-30', '2345678901', '789 Maple St'),
+        ('Bob', 'Brown', '1965-04-20', '8765432109', '101 Oak St');
+        ''')
+
+        # Insert data into the agents table
+        cur.execute('''
+        INSERT INTO agents (first_name, last_name, date_of_birth, phone_number)
+        VALUES
+        ('Charlie', 'Agent', '1985-07-11', '3456789012'),
+        ('Diana', 'Broker', '1979-11-25', '5678901234');
+        ''')
+
+        # Insert data into the contracts table
+        cur.execute('''
+        INSERT INTO contracts (property_id, client_id, owner_id, agent_id, price)
+        VALUES
+        (1, 1, 1, 1, 100000),
+        (2, 2, 2, 2, 150000);
+        ''')
+
+        # Commit the transaction
+        conn.commit()
+
+        print("Database seeded successfully.")
+
+    except psycopg2.Error as e:
+        # This catches PostgreSQL database related errors
+        print(f"An error occurred while seeding the database: {e}")
+    except Exception as e:
+        # This catches non-database related errors
+        print(f"An unexpected error occurred while seeding the database: {e}")
+    finally:
+        # Ensure that the cursor and connection are closed properly
+        if cur is not None:
+            cur.close()
+        if conn is not None:
+            conn.close()
 
 
 # show stuff
@@ -109,19 +179,29 @@ def show_property():
     conn = get_db_connection()
     cur = conn.cursor()
     cur.execute('SELECT * FROM properties;')
-    properties = cur.fetchall()
+    rows = cur.fetchall()
+    columns = [desc[0] for desc in cur.description]
+    properties = [dict(zip(columns, row)) for row in rows]
     cur.close()
     conn.close()
     return render_template('show_property.html', properties=properties)
 
 @app.route('/show_client')
 def show_client():
+    app.logger.debug('show_client')
     conn = get_db_connection()
     cur = conn.cursor()
     cur.execute('SELECT * FROM clients;')
-    clients = cur.fetchall()
+    rows = cur.fetchall()
+
+    columns = [desc[0] for desc in cur.description]
+
+    # Convert list of tuples to list of dicts
+    clients = [dict(zip(columns, row)) for row in rows]
+
     cur.close()
     conn.close()
+    app.logger.debug(clients)
     return render_template('show_client.html', clients=clients)
 
 @app.route('/show_owner')
@@ -129,7 +209,9 @@ def show_owner():
     conn = get_db_connection()
     cur = conn.cursor()
     cur.execute('SELECT * FROM owners;')
-    owners = cur.fetchall()
+    rows = cur.fetchall()
+    columns = [desc[0] for desc in cur.description]
+    owners = [dict(zip(columns, row)) for row in rows]
     cur.close()
     conn.close()
     return render_template('show_owner.html', owners=owners)
@@ -139,7 +221,9 @@ def show_agent():
     conn = get_db_connection()
     cur = conn.cursor()
     cur.execute('SELECT * FROM agents;')
-    agents = cur.fetchall()
+    rows = cur.fetchall()
+    columns = [desc[0] for desc in cur.description]
+    agents = [dict(zip(columns, row)) for row in rows]
     cur.close()
     conn.close()
     return render_template('show_agent.html', agents=agents)
@@ -149,7 +233,9 @@ def show_contract():
     conn = get_db_connection()
     cur = conn.cursor()
     cur.execute('SELECT * FROM contracts;')
-    contracts = cur.fetchall()
+    rows = cur.fetchall()
+    columns = [desc[0] for desc in cur.description]
+    contracts = [dict(zip(columns, row)) for row in rows]
     cur.close()
     conn.close()
     return render_template('show_contract.html', contracts=contracts)
@@ -261,9 +347,9 @@ def create_contract():
 
     return render_template('create_contract.html')
 
-# Drop all tables
 with app.app_context():
-    setup_db()
+    setup_db()  # Set up the database and create tables
+    seed_db()   # Seed the database with initial data
 
 if __name__ == '__main__':
     app.run(debug=True)
